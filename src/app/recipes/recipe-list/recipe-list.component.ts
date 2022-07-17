a37360cd0f80911483972c0391205ca66c350592
Marceldts <marceldeltoro23@gmail.com>
2022-07-17 12:42:38 +0200

    Integrados servicios Las recetas tienen un array de ingredientes Este array se puede añadir a la lista de la compra

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,20 +8,13 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  
-  recipes: Recipe[] = [
-    new Recipe('Receta de prueba', 'Esta receta es solo una prueba', 'https://live.staticflickr.com/5217/5460746911_4854236a6b_b.jpg'),
-    new Recipe('Otra receta de prueba', 'Esta receta sigue siendo solo una prueba', 'https://img.freepik.com/foto-gratis/espaguetis-italianos-salsa-tomate-sarten_1220-4240.jpg?w=2000')
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
 
-  onRecipeSelected(recipe:Recipe){
-    this.recipeWasSelected.emit(recipe);
-  }
 
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,27 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Recipe } from "./recipe.model";
+
+@Injectable()
+export class RecipeService{
+  recipeSelected = new EventEmitter<Recipe>();
+
+  constructor(private slService: ShoppingListService) {}
+
+  private recipes: Recipe[] = [
+  new Recipe('Hamburguesa', 'Hamburguesa con queso y huevo', 'https://img.freepik.com/foto-gratis/hamburguesa-vista-frontal-huevo-frito-tabla-cortar_23-2148784523.jpg?w=2000', [
+    new Ingredient('Pan de hamburguesa', 1), new Ingredient('Huevo', 1), new Ingredient('Loncha de queso', 2)
+   ]),
+  new Recipe('Espagueti', 'Espagueti con carne y huevo', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRx1OmEw4-tKpZxDKen5sRLqM8Rdx4Pn_n1-hxucZIzFYfi4F1A4fCldFLDFh5iSI3ZFpo&usqp=CAU', [
+    new Ingredient('Espagueti (en gramos)', 300), new Ingredient('Queso en polvo (en gramos)', 50), new Ingredient('Carne picada (en gramos)', 250), new Ingredient('Huevo', 2), new Ingredient('Bote de tomate frito', 1/3)
+  ])
+];
+
+getRecipes(){
+  return this.recipes.slice();
+}
+addIngredientsToShoppingList(ingredients: Ingredient[]) {
+   this.slService.addIngredients(ingredients);
+ }
+}
